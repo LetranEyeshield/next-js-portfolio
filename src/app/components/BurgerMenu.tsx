9b216b8d79a8f6074976c9a3417285a3fdafd5ef
YORIICHI <rosalinmichaelcris@gmail.com>
2025-07-27 22:17:09 +0800

    hiding the hamburger menu when link cliked

--- src/app/components/BurgerMenu.tsx
+++ src/app/components/BurgerMenu.tsx
@@ -4,18 +4,28 @@
 import { stack as Menu } from "react-burger-menu";
 import "./burger-stack.css"; // We'll create this file next
 import Link from "next/link";
+import { useState } from "react";
 
 export default function BurgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleStateChange = (state: { isOpen: boolean }) => {
+    setIsOpen(state.isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
   return (
-    <Menu right>
+    <Menu right isOpen={isOpen} onStateChange={handleStateChange}>
       {/* <Menu> */}
-      <Link href={"#about"} className="menu-item">
+      <Link href={"#about"} onClick={closeMenu} className="menu-item">
         About
       </Link>
-      <Link href={"#projects"} className="menu-item">
+      <Link href={"#projects"} onClick={closeMenu} className="menu-item">
         Projects
       </Link>
-      <Link href={"#socials"} className="menu-item">
+      <Link href={"#socials"} onClick={closeMenu} className="menu-item">
         Contact
       </Link>
     </Menu>
